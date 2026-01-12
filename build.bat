62eb89356e0fbd4690074f622e767e0976f8c660
Frater <13979047+saintfrater@users.noreply.github.com>
2026-01-12 10:25:58 +0100

    Remove build scripts and linker file

--- build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-cd src
-"c:\program files\NASM\nasm" -f bin boot.asm -o ..\build\bios.bin
-cd ..
-
-qemu-system-x86_64 -bios build/bios.bin -device loader,file=roms/vgabios.bin,addr=0xC0000,force-raw=on -device isa-debugcon,chardev=myconsole -chardev stdio,id=myconsole -k be -display sdl
